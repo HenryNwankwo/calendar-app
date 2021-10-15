c898e23dbba7ae644b57837c6b36213d25c8222c
Nwankwo Henry <nameisifunanya@gmail.com>
2021-10-15 15:44:02 +0100

    Added custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-color': '#00B87C',
+        'secondary-color': '#1A61DB',
+        'error-color': '#F40101',
+        'success-color': '#008B5E',
+        'success-color2': '#FFB425',
+        'line-color': '#F6F6F6',
+        'light-green': '#E1FDF4',
+        'bg-color-grey': '#F6F6F6',
+        'bg-color-white': '#FFFFFF',
+      },
+    },
   },
   variants: {
     extend: {},
