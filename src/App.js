d5523f225b07b04f3f263a62d07ead53e289f2f9
Idris adekunle <idrisadekunle726@gmail.com>
2021-10-20 09:53:44 +0100

    removed App.css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import CalendarApp from './pages/CalendarApp';
-<<<<<<< HEAD
-import './App.css';
 import CreateEvent from './components/CreateEvent/CreateEvent';
-=======
-
 //Creating an app context using context API
 export const AppContext = React.createContext();
->>>>>>> 9540d948e45d45e936f2e33ef244edadb84081dc
-
 function App() {
   const [month, setMonth] = useState(
     new Date().toLocaleString({}, { month: 'long' })
