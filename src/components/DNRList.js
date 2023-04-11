a1332b005a8e476fafb9fa55ac74c35c9e4e81e1
Nwankwo Henry <nameisifunanya@gmail.com>
2023-04-11 15:53:14 +0100

    Customized the react-select option dropdown

--- src/components/DNRList.js
+++ src/components/DNRList.js
@@ -1,11 +1,45 @@
-import React, { useContext, useEffect, useRef } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import { AppContext } from '../App';
-
+import Select from 'react-select';
+import { options } from '../assets/dnrOptions';
+import { RiArrowDropDownFill } from 'react-icons/ri';
 function DNRList() {
+  const [dNRValue, setDNRValue] = useState(null);
   return (
     <>
-      <div className='h-auto w-57 rounded shadow-md py-3 absolute bg-white top-7 left-1 z-50'>
-        <ul className='text-sm text-txt-color'>
+      <div className='h-auto w-56 rounded py-3 absolute bg-white top-7 left-1 z-50'>
+        <Select
+          options={options}
+          menuPlacement='auto'
+          defaultValue={options[0]}
+          placeholder='Do not Repeat'
+          onChange={setDNRValue}
+          className='text-sm text-txt-color customSelect'
+          styles={{
+            control: (baseStyle, state) => ({
+              border: 0,
+              outlineColor: state.isFocused ? 'red' : 'green',
+              width: '176px',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              ':before': {
+                fontFamily: 'Font Awesome 6 Free',
+                content: '"\f2f1"',
+              },
+            }),
+            option: (baseStyle, state) => ({
+              width: '100',
+              height: 'auto',
+              padding: '4px 16px 4px 16px',
+              ':hover': {
+                backgroundColor: '#E1FDF4',
+                cursor: 'pointer',
+              },
+            }),
+          }}
+        ></Select>
+        {/*  <ul className='text-sm text-txt-color'>
           <li className='px-4 py-1 hover:cursor-pointer hover:text-black hover:bg-light-green'>
             Do not repeat
           </li>
@@ -27,7 +61,7 @@ function DNRList() {
           <li className='px-4 py-1 hover:cursor-pointer hover:text-black hover:bg-light-green'>
             Custom...
           </li>
-        </ul>
+        </ul> */}
       </div>
     </>
   );
