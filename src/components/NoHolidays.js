a8b77fa9ee97578c536e1ccdf3ba4287d329d625
Nwankwo Henry <nameisifunanya@gmail.com>
2023-05-02 16:53:01 +0100

    Created the structure for No Holidays components

--- src/components/NoHolidays.js
+++ src/components/NoHolidays.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AppContext } from '../App';
 
 function NoHolidays() {
-  return <div></div>;
+  const { holidayList } = useContext(AppContext);
+  return (
+    <div className='w-full h-auto bg-line-color flex flex-row'>
+      <div className='flex justify-center items-center h-96 w-full'>
+        <h5 className='text-txt-color text-center font-bold'>
+          No Holidays this month
+        </h5>
+      </div>
+
+      <div>
+        <p className='text-lg mb-3'>Upcoming Holidays</p>
+        <div></div>
+      </div>
+    </div>
+  );
 }
 
 export default NoHolidays;
