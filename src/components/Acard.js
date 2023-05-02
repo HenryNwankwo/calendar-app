371c295a5102f71509ad4e31e73e24c2669b123c
Nwankwo Henry <nameisifunanya@gmail.com>
2023-05-02 17:13:48 +0100

    Structured and Created a single card component

---
+++ src/components/Acard.js
@@ -0,0 +1,83 @@
+import React, { useContext, useRef } from 'react';
+import { AppContext } from '../App';
+import { RiDeleteBinLine, RiNotification2Line } from 'react-icons/ri';
+import { FiEdit2 } from 'react-icons/fi';
+
+function Acard({ holiday }) {
+  const {
+    setCurrentEventID,
+    deleteEventPopUp,
+    setDeleteEventPopUp,
+    clickedEvent,
+    setClickedEvent,
+    showEditEvent,
+    setShowEditEvent,
+  } = useContext(AppContext);
+  //Calls an event delete pop up and assigns its id to a state
+  const deleteEventMethod = (id) => {
+    setDeleteEventPopUp(!deleteEventPopUp);
+    setCurrentEventID(id);
+  };
+
+  //state that determines the view state of an event details
+  const currentEventRef = useRef();
+
+  const clickedEventHandler = (id) => {
+    setClickedEvent(!clickedEvent);
+    setCurrentEventID(id);
+  };
+
+  //Edit event handler
+
+  const editEventHandler = (id) => {
+    setShowEditEvent(!showEditEvent);
+    setCurrentEventID(id);
+  };
+  return (
+    <>
+      <div
+        className='flex flex-col bg-bg-color-white mt-1.5 mx-4 md:mx-7 p-4 md:p-6 hover:bg-gray-50 hover:cursor-pointer shadow-md select-none'
+        style={{ borderLeft: `8px solid ${holiday.color}` }}
+        key={holiday.id}
+        ref={currentEventRef}
+        onClick={() => clickedEventHandler(holiday.id)}
+      >
+        <div className='flex justify-between mb-2'>
+          {/* Event Title */}
+          <span className='flex flex-row w-auto items-center'>
+            <h6 className='font-bold text-lg'>{holiday.eventTitle}</h6>
+            <span className='hoverRoundedGray ml-4 '>
+              <RiNotification2Line className='text-xl hover:cursor-pointer text-primary-color' />
+            </span>
+          </span>
+          <span className='flex flex-row w-auto items-center'>
+            <span
+              className='hoverRoundedGray mr-1'
+              onClick={() => editEventHandler(holiday.id)}
+            >
+              <FiEdit2 className='text-xl text-primary-color' />
+            </span>
+            <span
+              className='hoverRoundedGray'
+              onClick={() => deleteEventMethod(holiday.id)}
+            >
+              <RiDeleteBinLine className='text-xl hover:cursor-pointer text-primary-color' />
+            </span>
+          </span>
+        </div>
+        {/* Event Color */}
+        <p className='text-sm' style={{ color: holiday.color }}>
+          {holiday.allDay
+            ? 'All Day'
+            : `${holiday.startTime} - ${holiday.endTime}`}
+        </p>
+        {/* Event Description */}
+        <p className='font-bold text-sm' style={{ color: holiday.color }}>
+          {holiday.description}
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default Acard;
--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -1,22 +1,16 @@
-import React, { useContext, useRef } from 'react';
-import { RiDeleteBinLine, RiNotification2Line } from 'react-icons/ri';
-import { FiEdit2 } from 'react-icons/fi';
+import React, { useContext} from 'react';
 import { startOfMonth, endOfMonth, eachDayOfInterval } from 'date-fns';
 import { AppContext } from './../App';
 import DeleteEvent from './DeleteEvent';
 import EditEvent from './EditEvent';
 import ShowEventPopUp from './ShowEventPopUp';
+import Acard from './Acard';
 function EventCard({ theHolidayList }) {
   const {
     filteredHolidayList,
-    currentEventID,
-    setCurrentEventID,
-    deleteEventPopUp,
-    setDeleteEventPopUp,
-    clickedEvent,
-    setClickedEvent,
-    showEditEvent,
-    setShowEditEvent,
+    currentEventID,    
+    deleteEventPopUp,    
+    clickedEvent,  
     year,
     month,
   } = useContext(AppContext);
@@ -31,26 +25,7 @@ function EventCard({ theHolidayList }) {
     end: monthEndDate,
   });
 
-  //Calls an event delete pop up and assigns its id to a state
-  const deleteEventMethod = (id) => {
-    setDeleteEventPopUp(!deleteEventPopUp);
-    setCurrentEventID(id);
-  };
-
-  //state that determines the view state of an event details
-  const currentEventRef = useRef();
-
-  const clickedEventHandler = (id) => {
-    setClickedEvent(!clickedEvent);
-    setCurrentEventID(id);
-  };
-
-  //Edit event handler
-
-  const editEventHandler = (id) => {
-    setShowEditEvent(!showEditEvent);
-    setCurrentEventID(id);
-  };
+  
 
   return (
     <div className=''>
@@ -59,50 +34,7 @@ function EventCard({ theHolidayList }) {
           {filteredHolidayList
             .filter((event) => event.startDate.getDate() === day.getDate())
             .map((holiday) => (
-              <div
-                className='flex flex-col bg-bg-color-white mt-1.5 mx-4 md:mx-7 p-4 md:p-6 hover:bg-gray-50 hover:cursor-pointer shadow-md select-none'
-                style={{ borderLeft: `8px solid ${holiday.color}` }}
-                key={holiday.id}
-                ref={currentEventRef}
-                onClick={() => clickedEventHandler(holiday.id)}
-              >
-                <div className='flex justify-between mb-2'>
-                  {/* Event Title */}
-                  <span className='flex flex-row w-auto items-center'>
-                    <h6 className='font-bold text-lg'>{holiday.eventTitle}</h6>
-                    <span className='hoverRoundedGray ml-4 '>
-                      <RiNotification2Line className='text-xl hover:cursor-pointer text-primary-color' />
-                    </span>
-                  </span>
-                  <span className='flex flex-row w-auto items-center'>
-                    <span
-                      className='hoverRoundedGray mr-1'
-                      onClick={() => editEventHandler(holiday.id)}
-                    >
-                      <FiEdit2 className='text-xl text-primary-color' />
-                    </span>
-                    <span
-                      className='hoverRoundedGray'
-                      onClick={() => deleteEventMethod(holiday.id)}
-                    >
-                      <RiDeleteBinLine className='text-xl hover:cursor-pointer text-primary-color' />
-                    </span>
-                  </span>
-                </div>
-                {/* Event Color */}
-                <p className='text-sm' style={{ color: holiday.color }}>
-                  {holiday.allDay
-                    ? 'All Day'
-                    : `${holiday.startTime} - ${holiday.endTime}`}
-                </p>
-                {/* Event Description */}
-                <p
-                  className='font-bold text-sm'
-                  style={{ color: holiday.color }}
-                >
-                  {holiday.description}
-                </p>
-              </div>
+              <Acard holiday={holiday} />
             ))}
         </div>
       ))}
